1d88e119c64799ff94ece4906105dab89c7f8cde
George <georgesims21@hotmail.com>
2020-06-14 11:25:06 +0200

    Created tree node, with basic useage in the .c file

--- src/tree.c
+++ src/tree.c
@@ -1,8 +1,13 @@
 #include "tree.h"
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    NODE *test;
-    test->name = "hello";
-    return 0;
-}
+//int main(int argc, char *argv[]) {
+//    NODE *test;
+//    memset(test, 0, sizeof(struct node));
+//    test->type = 'F';
+//    strncpy(test->name, "testing", strlen("testing"));
+//    strncpy(test->name,  "hello", strlen("hello"));
+//    printf("The node hasvalue: %s", test->name);
+//    return 0;
+//}
