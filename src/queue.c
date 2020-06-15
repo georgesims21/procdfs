d9f1b2371ad654886b8f56364ffcadf596bb53a3
George <georgesims21@hotmail.com>
2020-06-15 15:16:04 +0200

    Cleaned up and added complete .h queue file

--- src/queue.c
+++ src/queue.c
@@ -1,14 +1,11 @@
 #include "queue.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int enqueue(QUEUE **queue, NODE *node) {
 /*
  * TODO
  *   * Error check the malloc with errno
  */
-    QUEUE *qp = queue;
+    QUEUE *qp = *queue;
     QUEUE *new_elem = (QUEUE *)malloc(sizeof(QUEUE));
     if(!new_elem) {
         // unsuccessful malloc
@@ -36,11 +33,12 @@ NODE dequeue(QUEUE **queue) {
  *   * Need to error check when queue is empty
  */
     QUEUE *qp = *queue;
+    NODE *elem = NULL;
     if(!qp) {
         // Empty queue
         printf("queue already empty!");
     } else {
-        NODE *elem = qp->node;
+        elem = qp->node;
         free(qp->node);
         if(qp->next) {
             *queue = qp->next;
@@ -48,9 +46,10 @@ NODE dequeue(QUEUE **queue) {
             *queue = NULL;
         }
         printf("Dequeued NODE: %s\n", elem->name);
-        return *elem;
     }
+    return *elem;
 }
+
 void printq(QUEUE **queue) {
 
     QUEUE *qp = *queue;
@@ -94,35 +93,35 @@ int lenq(QUEUE **queue) {
     return count;
 }
 
-int main(int argc, char *argv[]) {
-/*
- * TODO
- */
-    QUEUE *test = {NULL};
-    printq(&test);
-    NODE *nn = (NODE *)malloc(sizeof(NODE));
-    NODE *n2 = (NODE *)malloc(sizeof(NODE));
-    strcpy(nn->name,"testing");
-    strcpy(n2->name, "Another one");
-    enqueue(&test, nn);
-    enqueue(&test, n2);
-    printq(&test);
-
-    printf("queue length = %d\n", lenq(&test));
-
-    dequeue(&test);
-    printq(&test);
-
-    NODE *n3 = (NODE *)malloc(sizeof(NODE));
-    NODE *n4 = (NODE *)malloc(sizeof(NODE));
-    strcpy(n3->name,"third");
-    strcpy(n4->name, "fourth");
-    enqueue(&test, n3);
-    enqueue(&test, n4);
-    printq(&test);
-    printf("queue length = %d\n", lenq(&test));
-
-    dealloq(&test);
-
-    return 0;
-}
+//int main(int argc, char *argv[]) {
+///*
+// * TODO
+// */
+//    QUEUE *test = {NULL};
+//    printq(&test);
+//    NODE *nn = (NODE *)malloc(sizeof(NODE));
+//    NODE *n3 = (NODE *)malloc(sizeof(NODE));
+//    strcpy(nn->name,"testing");
+//    strcpy(n3->name, "Another one");
+//    enqueue(&test, nn);
+//    enqueue(&test, n3);
+//    printq(&test);
+//
+//    printf("queue length = %d\n", lenq(&test));
+//
+//    dequeue(&test);
+//    printq(&test);
+//
+//    NODE *n4 = (NODE *)malloc(sizeof(NODE));
+//    NODE *n5 = (NODE *)malloc(sizeof(NODE));
+//    strcpy(n4->name,"third");
+//    strcpy(n5->name, "fourth");
+//    enqueue(&test, n4);
+//    enqueue(&test, n5);
+//    printq(&test);
+//    printf("queue length = %d\n", lenq(&test));
+//
+//    dealloq(&test);
+//
+//    return 1;
+//}
--- src/queue.h
+++ src/queue.h
@@ -2,17 +2,42 @@
 #define PROCSYS_QUEUE_H
 
 #include "tree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct queue {
     struct queue *next;
     struct node *node;
 } QUEUE;
 
-/* Push a tree NODE into the queue
+/* Enqueue a NODE into the queue
  * @return
  * 0 upon success
  * -1 upen failiure
  */
 int enqueue(QUEUE **queue, NODE *node);
+
+/* Dequeue a NODE from the queue
+ * @return
+ * NODE located at front of the queue
+ */
+NODE dequeue(QUEUE **queue);
+
+/* Print queue contents */
+void printq(QUEUE **queue);
+
+/* Deallocate (free) all heap memory in queue
+ * @return
+ * 0 upon success
+ * -1 upon failiure
+ */
+int dealloq(QUEUE **queue);
+
+/* Return the length of the queue
+ * @return
+ * int containing length of queue
+ */
+int lenq(QUEUE **queue);
 
 #endif //PROCSYS_QUEUE_H
