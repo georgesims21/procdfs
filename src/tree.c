4e74d078bca10b0c2475671379764c505c8e5ed5
George <georgesims21@hotmail.com>
2020-06-15 18:46:08 +0200

    Added create node function with description in .h

--- src/tree.c
+++ src/tree.c
@@ -1,13 +1,49 @@
 #include "tree.h"
-#include <stdio.h>
-#include <string.h>
+/*                      DIR/file
+              / - root
+              \ - sibling
+             A(/A)
+    child - / \
+     (/A/C)C   B(/B)
+          /     \
+  (/A/C/d)d      e(/e)
 
-//int main(int argc, char *argv[]) {
-//    NODE *test;
-//    memset(test, 0, sizeof(struct node));
-//    test->type = 'F';
-//    strncpy(test->name, "testing", strlen("testing"));
-//    strncpy(test->name,  "hello", strlen("hello"));
-//    printf("The node hasvalue: %s", test->name);
-//    return 0;
-//}
+  TODO
+    * Root doesn't have parent or sibling
+    * Must order the siblings alphabetically
+    * Search: follow_sibling to check siblings/stay same level
+              follow_child to enter into that dir
+ */
+
+NODE *create_node(char df, char name[], NODE *pnt, NODE *cld, NODE *sib) {
+/* TODO
+ *   * Need a MAXNAME var to control name before giving to this function
+ */
+    NODE *new_node = (NODE *)malloc(sizeof(NODE));
+    strncpy(new_node->name, name, strlen(name));
+    new_node->type = df;
+
+    if(pnt) {
+        pnt->child = new_node;
+    }
+    if(cld) {
+        cld->parent = new_node;
+    }
+    if(sib) {
+        sib->sibling = new_node;
+    }
+    new_node->parent = pnt;
+    new_node->child = cld;
+    new_node->sibling = sib;
+
+    return new_node;
+}
+
+int main(int argc, char *argv[]) {
+    NODE *root = create_node('D', "/", 0, 0, 0);
+    NODE *dirA = create_node('D', "A", root, 0, 0);
+
+    printf("dirA's parent is: %s\n", dirA->parent->name);
+    printf("root's child is: %s\n", root->child->name);
+    return 0;
+}
--- src/tree.h
+++ src/tree.h
@@ -1,16 +1,29 @@
 #ifndef PROCSYS_TREE_H
 #define PROCSYS_TREE_H
 
-#include "queue.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAXPATH 64
-
-extern struct node *root;
 
 typedef struct node {
     struct node *parent, *child, *sibling;
     char type; // F or D
     char name[MAXPATH];
 } NODE;
+
+/* Create a new node. Will update it's parent/child/sibling nodes
+ * if !NULL
+ * @input
+ * df: char either 'D' for directory or 'F' for file
+ * name: the directory/file name for this NODE
+ * pnt: the parent NODE, 0 or NULL if unknown
+ * cld: the child NODE, 0 or NULL if unknown
+ * sib: the sibling NODE, 0 or NULL if unknown
+ * @return
+ * NODE type
+ */
+NODE *create_node(char df, char name[], NODE *pnt, NODE *cld, NODE *sib);
 
 #endif //PROCSYS_TREE_H
