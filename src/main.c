0b2ec13429a1c1eae990832abc398deb08f1137e
George <georgesims21@hotmail.com>
2020-06-17 16:01:36 +0200

    Updated readdir_callback, plenty of errors but related to different file types not implemented

--- src/main.c
+++ src/main.c
@@ -1,134 +1,11 @@
-#define FUSE_USE_VERSION 26
-
-#include <fuse.h>
-#include <string.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-
-#define BUFSIZE 1024
-#define MAXPATH 64
-
-/* TODO
- *  * Must use BST to mirror procfs
- *  * Use ds to populate in for loop
-*/
-
-static const char *pndpath = "/proc/net/dev";
-static char proc[] = "/proc";
-
-int file_exists(const char *pathname) {
-    int fd;
-    if((fd = openat(AT_FDCWD, pathname, O_RDONLY)) < 0)
-        // file doesn't exist
-        return -1;
-    return 0;
-}
-
-// All files in /proc are 0 in size, so must access the files and count chars manually
-int procsize(const char *pathname) {
-
-    char buf[BUFSIZE];
-    int fd, count = 0;
-    if((fd = openat(AT_FDCWD, pathname, O_RDONLY)) < 0)
-        // file doesn't exist
-        return -1;
-    while(read(fd, buf, 1) > 0) {
-        count++;
-    }
-    close(fd);
-    return count;
-}
-
-// Return char data from given file
-int datafetch(char *buffer, const char *pathname) {
-/* TODO
- *  * Error handling
- *  * Malloc the buf
- */
-    int filesize;
-    if((filesize = procsize(pathname)) < 1)
-        return -1;
-    char *mal = (char *) malloc(filesize + 1);
-    int fd = openat(AT_FDCWD, pathname, O_RDONLY);
-    read(fd, mal, filesize);
-    mal[filesize + 1] = '\0';
-    close(fd);
-    snprintf(buffer, filesize, "%s", strdup(mal));
-    free(mal);
-    return 0;
-}
-
-int is_file(const char *path) {
-
-    struct stat st;
-    if((stat(path, &st)) < 0)
-        return -1;
-
-    return S_ISREG(st.st_mode);
-}
-
-int is_dir(const char *path) {
-
-    struct stat st;
-    if((stat(path, &st)) < 0)
-        return -1;
-
-    return S_ISDIR(st.st_mode);
-}
-
-mode_t what_am_i(const char *path) {
-    struct stat st;
-    if((stat(path, &st)) == -1)
-        perror("stat");
-
-    return st.st_mode & S_IFMT;
-}
-
-struct stat *retstat(const char *path) {
-    struct stat *tmpstbuf;
-        if(stat(path, &tmpstbuf) == -1)
-            printf("stat failed");
-    return tmpstbuf;
-}
-
-
-// Given a filepath, fill the buf with its contents
-size_t populate(char **buf, size_t size, off_t offset, const char *path) {
-/* TODO
- */
-    int len = procsize(path);
-    char filecontent[BUFSIZE];
-    if(datafetch(filecontent, path) < 0) {
-        return -1;
-    }
-    if (offset >= len) {
-        return 0;
-    }
-    if (offset + size > len) {
-        memcpy(*buf, filecontent + offset, len - offset);
-        return len - offset;
-    }
-    memcpy(*buf, filecontent + offset, size);
-    return size;
-}
-
-const char *add_proc(const char *path) {
-
-    char *final_path = (char *) malloc(strlen(proc) + strlen(path));
-    strcpy(final_path, proc);
-    strcat(final_path, path);
-    return final_path;
-}
-
+#include "fileops.h"
 
 static int getattr_callback(const char *path, struct stat *stbuf) {
 /* TODO
- *   * Check out the other struct elements and find out how to find them:
+ *   * Check out the other struct stat elements and find out how to use them:
  *                    https://linux.die.net/man/2/stat
+ *   * Also the fuse.h file gives more detail on each function:
+ *                    https://github.com/libfuse/libfuse/blob/master/include/fuse.h
  */
     memset(stbuf, 0, sizeof(struct stat));
     const char *final_path = add_proc(path);
@@ -147,17 +24,26 @@ static int getattr_callback(const char *path, struct stat *stbuf) {
 }
 
 static int readdir_callback(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
-    (void) offset;
-    (void) fi;
+    (void) offset; // To stop compiler warnings on unused variables
+    (void) fi; // fi contains a file handle to be used in create, open and opendir
 
-    filler(buf, ".", NULL, 0);
-    filler(buf, "..", NULL, 0);
-    if(strcmp(path, "/") == 0) {
-      filler(buf, "net", NULL, 0);
+    const char *final_path = add_proc(path);
+    int dir_f_size =  dir_size(final_path);
+    char *dir_cnt[dir_f_size];
+    print_dir(final_path, dir_cnt);
+
+    for (int i = 0; i < dir_f_size; i++) {
+       filler(buf, dir_cnt[i], NULL, 0);
     }
-    if(strcmp(path, "/net") == 0) {
-      filler(buf, "dev", NULL, 0);
-    }
+
+//    filler(buf, ".", NULL, 0);
+//    filler(buf, "..", NULL, 0);
+//    if(strcmp(path, "/") == 0) {
+//      filler(buf, "net", NULL, 0);
+//    }
+//    if(strcmp(path, "/net") == 0) {
+//      filler(buf, "dev", NULL, 0);
+//    }
     return 0;
 }
 
@@ -173,7 +59,7 @@ static int read_callback(const char *path, char *buf, size_t size, off_t offset,
     return final_size;
 }
 
-static struct fuse_operations fuse_example_operations = {
+static struct fuse_operations procsys_ops = {
     .getattr = getattr_callback,
     .open = open_callback,
     .read = read_callback,
@@ -181,5 +67,5 @@ static struct fuse_operations fuse_example_operations = {
 };
 
 int main(int argc, char *argv[]) {
-    return fuse_main(argc, argv, &fuse_example_operations, NULL);
+    return fuse_main(argc, argv, &procsys_ops, NULL);
 }
