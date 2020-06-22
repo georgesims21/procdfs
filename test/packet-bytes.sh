1af2bf261d71f68b6e847829d133d5ce3ba005c1
George <georgesims21@hotmail.com>
2020-06-22 11:00:28 +0200

    Completed ifstat script

--- test/packet-bytes.sh
+++ test/packet-bytes.sh
@@ -15,26 +15,12 @@ ret_arr() {
    echo $(cat $pnetd | grep $1 | awk '{print $11}') # pTX
 }
 
-print_res() {
-    #1: device name
-    #2: bRX    #3: bTX
-    #4: pRX    #5: pTX
-
-    #echo "$1\t: RX Pkts/s:\t" "$4\t" "TX Pkts/s:\t" "$5\t" "RX Data/s:\t" "$2\t" "TX Data/s:\t" "$3\t"
-    if(( $1 == $wlp ))
-    then
-        echo "$1: $4\t $5\t\t $2\t\t $3\t\t "
-    else
-        echo "$1: $4\t\t $5\t\t $2\t\t $3\t\t "
-    fi
-}
-
 print_rows() {
     #1: device name
     #2: bRX    #3: bTX
     #4: pRX    #5: pTX
 
-    echo "\tRX Pkts/s\t" "TX Pkts/s:\t" "RX Data/s:\t" "TX Data/s:\t"
+    echo "\t\tRX Pkts/s\t" "TX Pkts/s\t" "RX Data/s\t" "TX Data/s\t"
 }
 
 echo -e "\033[?25l\033[2J\033[0;0H IFSTAT\n ------\n #kernel\n" # hide cursor & clear screen
@@ -63,9 +49,9 @@ while true; do
     t_pRX=$(( ${tun_post[2]} - ${tun_pre[2]} ))
     t_pTX=$(( ${tun_post[3]} - ${tun_pre[3]} ))
 
-    echo -e "\r\033[4;0H\033[2K $(print_rows)\n"\
-        "\033[2K$(print_res $lo $l_bRX $l_bTX $l_pRX $l_pTX)\n"\
-        "\033[2K$(print_res $wlp $w_bRX $w_bTX $w_pRX $w_pTX)\n"\
-    "\033[2K$(print_res $tun $t_bRX $t_bTX $t_pRX $t_pTX)\t"
+    echo -e "\r\033[4;0H\033[2K $(print_rows)\n"                        \
+            "\033[2K$lo\t\t $l_bRX\t\t $l_bTX\t\t $l_pRX\t\t $l_pTX\n"  \
+            "\033[2K$wlp\t $w_bRX\t\t $w_bTX\t\t $w_pRX\t\t $w_pTX\n"   \
+            "\033[2K$tun\t\t $t_bRX\t\t $t_bTX\t\t $t_pRX\t\t $t_pTX\t"
 
 done
