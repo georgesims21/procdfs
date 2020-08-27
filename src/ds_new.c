7c428b5902fb96f5ed5a3c9f24ead3ea6e620061
George Sims <georgesims21@hotmail.com>
2020-08-27 13:25:13 +0200

    Linked list methods working, some valgrind errors on the realloc but not sure why

--- src/ds_new.c
+++ src/ds_new.c
@@ -1,23 +1,135 @@
 #include "ds_new.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int req_tracker_ll_add(Request_tracker_node **rtn, Request *req) {
+int req_tracker_ll_add(Request_tracker_node **head, Request *req) {
 
     if(req == NULL)
         return -1;
-
-    Request_tracker_node *rtnptr = *rtn;
-    if(rtnptr->req == NULL) {
-        // empty ll
-        rtnptr->req = req;
-        rtnptr->next = NULL;
-    } else {
-        while(rtnptr->next != NULL)
-            rtnptr = rtnptr->next;
-        rtnptr->next->req = req;
-        rtnptr->next->next = NULL;
+    Request_tracker_node *listptr = *head;
+    Request_tracker_node *newnode = (Request_tracker_node *)malloc(sizeof(Request_tracker_node));
+    if(newnode == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
     }
-
+    memset(newnode, 0, sizeof(Request_tracker_node));
+    newnode->req = req;
+    newnode->next = NULL;
+    if(listptr == NULL) {
+        // create new head of list
+        *head = newnode;
+    } else {
+        // append to end of list
+        while(listptr->next != NULL)
+            listptr = listptr->next;
+        listptr->next = newnode;
+    }
     return 0;
+}
+
+int request_cmp(Request req1, Request req2) {
+
+    if (req1.atomic_counter == req2.atomic_counter &&
+            strncmp(req1.path, req2.path, strlen(req1.path)) == 0 &&
+            req1.buflen == req2.buflen &&
+            req1.sender.sock == req2.sender.sock &&
+            req1.sender.addr.sin_addr.s_addr == req2.sender.addr.sin_addr.s_addr &&
+            req1.sender.addr.sin_addr.s_addr == req2.sender.addr.sin_addr.s_addr) {
+        return 0;
+    }
+    return 1;
+}
+
+int req_add_content(Request_tracker_node **head, Request req, char *cnt, int cntlen) {
+    // assumes user gave size INCLUDING '\0'
+
+    Request_tracker_node *node = req_tracker_ll_fetch(head, req);
+    if(node == NULL)
+        return -1;
+    node->req = realloc(node->req, sizeof(Request) + (cntlen * sizeof(char)));
+    if(node->req == NULL) {
+        perror("realloc");
+        exit(EXIT_FAILURE);
+    }
+    snprintf(node->req->buf, cntlen, "%s", cnt);
+    node->req->buflen = cntlen;
+    return 0;
+}
+
+Request_tracker_node *req_tracker_ll_fetch(Request_tracker_node **head, Request req) {
+
+    if(head == NULL)
+        return NULL;
+    Request_tracker_node *listptr = *head;
+    while(listptr != NULL) {
+        if(request_cmp(*listptr->req, req) == 0) {
+            return listptr;
+        }
+        listptr = listptr->next;
+    }
+    return NULL;
+}
+
+void req_tracker_ll_print(Request_tracker_node **head) {
+
+    int count = 0;
+    Request_tracker_node *listptr = *head;
+
+    if(listptr != NULL) {
+        while (listptr != NULL) {
+            printf("req_tracker_ll[%d]:\nAtomic counter: %d\n"
+                   "File path: %s\n"
+                   "Socket: %d\n"
+                   "IP: %u\n"
+                   "Port: %u\n"
+                   "Buflen: %lu\n"
+                   "Sender buf: %s\n"
+                   "\n",
+                   count,
+                   listptr->req->atomic_counter,
+                   listptr->req->path,
+                   listptr->req->sender.sock,
+                   listptr->req->sender.addr.sin_addr.s_addr,
+                   listptr->req->sender.addr.sin_addr.s_addr,
+                   listptr->req->buflen,
+                   (listptr->req->buflen > 0) ? listptr->req->buf : ""
+            );
+            listptr = listptr->next;
+            count++;
+        }
+        return;
+    }
+    printf("List empty!\n");
+}
+
+int request_ll_free(Request_tracker_node **head) {
+
+    Request_tracker_node *reqptr = *head;
+    Request_tracker_node *next;
+    if(reqptr == NULL) {
+        printf("The list is empty! Not freeing anything\n");
+        return -1;
+    }
+    while(reqptr != NULL) {
+        next = reqptr->next;
+        reqptr->next = NULL;
+        free(reqptr);
+        printf("Request freed!\n");
+        reqptr = next;
+    }
+    return 0;
+}
+
+void inprog_free(Inprog *inp) {
+
+    Request_tracker_node *reqptr = inp->req_ll_head;
+    while(reqptr->req != NULL) {
+        Request_tracker_node *next = reqptr->next;
+        free(reqptr->req);
+        reqptr->next = NULL;
+        reqptr = next;
+    }
+    free(inp);
 }
--- src/ds_new.h
+++ src/ds_new.h
@@ -3,6 +3,7 @@
 
 #include <glob.h>
 #include <netinet/in.h>
+#include <stdbool.h>
 
 #define MAXPATH 64
 
@@ -14,9 +15,9 @@ typedef struct address {
 
 typedef struct request {
     Address sender;
-    char received;
     int atomic_counter;
     char path[MAXPATH];
+    long buflen;
     char buf[]; // to allow for flexible array member
 }Request;
 
@@ -28,7 +29,7 @@ typedef struct request_tracker_node {
 typedef struct inprog {
     int atomic_counter;
     int messages_sent;
-    char complete;
+    bool complete;
     Request_tracker_node *req_ll_head; // head of linked list of the machines who got message
 }Inprog;
 
@@ -38,6 +39,11 @@ typedef struct inprog_tracker_node {
 }Inprog_tracker_node;
 
 
-int req_tracker_ll_add(Request_tracker_node **rtn, Request *req);
+int req_tracker_ll_add(Request_tracker_node **head, Request *req);
+void req_tracker_ll_print(Request_tracker_node **head);
+int req_add_content(Request_tracker_node **head, Request req, char *cnt, int cntlen);
+Request_tracker_node *req_tracker_ll_fetch(Request_tracker_node **head, Request req);
+void inprog_free(Inprog *inp);
+int request_ll_free(Request_tracker_node **head);
 
 #endif //PROCSYS_DS_NEW_H
